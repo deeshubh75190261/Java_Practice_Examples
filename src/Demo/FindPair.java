991d77298b667f9d5fb7324a1f798b043fe31e84
Deepak Patil <deeshubh75190261>
2026-03-16 21:27:55 +0530

    Commit 5: Change in JRE .

--- src/Demo/FindPair.java
+++ src/Demo/FindPair.java
@@ -1,12 +1,14 @@
 package Demo;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindPair {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 0, 1, 5, 6, 2, 5, 11, 15 };
+		int[] arr = { 1, 5, 46, 2, 6, 11, 15 };
 		int target = 9;
 		boolean flag = false;
 		for (int i = 0; i < arr.length; i++) {
@@ -26,6 +28,27 @@ public class FindPair {
 			System.out.println("Pair not found then : " + Arrays.toString(arr));
 		}
 
+		System.out.println("====================================================");
+
+		Map<Integer, Integer> pairMap = new HashMap<>();
+
+		boolean flage = false;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			int secValue = target - arr[i];
+
+			if (pairMap.containsKey(secValue)) {
+				flage = true;
+				System.out.println(Arrays.toString(new int[] { secValue, arr[i] }));
+				break;
+			}
+			pairMap.put(arr[i], i);
+		}
+
+		if (!flage) {
+			System.out.println(Arrays.toString(new int[] {}));
+		}
 	}
 
 }
